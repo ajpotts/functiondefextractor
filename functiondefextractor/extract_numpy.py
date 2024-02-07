67d7bacac31963e4c7b3a3cd7ea57ca66c0a7902
Amanda Potts <ajpotts@users.noreply.github.com>
2024-02-07 14:05:40 -0500

    working

--- functiondefextractor/extract_numpy.py
+++ functiondefextractor/extract_numpy.py
@@ -6,22 +6,38 @@ from core_extractor import get_report
 from extractor_cmd import validate_inputs
 from extractor_cmd import create_parser
 
+import pandas as pd
+
 if __name__ == '__main__':
 
     scipy_path = "/home/amandapotts/git/scipy/scipy"
     print(scipy_path)
 
+    #   Extract the function calls, and enhance with extracted function names.
     df = extractor(scipy_path)
 
     df['np_calls'] = df['Code'].str.findall(r"np\.([\w\.]*)")
     df['num_np_calls'] = df['np_calls'].apply(lambda x: len(x))
     df['np_calls_w_args'] = df['Code'].str.findall(r"np\.([\w\.]*\([\s\w=_]*\))")
-    df['function'] = df['Uniq ID'].str.findall(r"/home/amandapotts/git/scipy/scipy/([\w_/]*)")
-    df = df.explode('function')
-
+    df['scipy_function'] = df['Uniq ID'].str.findall(r"/home/amandapotts/git/scipy/scipy/([\w_/]*)")
+    df = df.explode('scipy_function')
 
     my_out = "/home/amandapotts/git/functiondefextractor/data/scipy/scipy_function_definitions.csv"
-
-    print(df)
-
     df.to_csv(my_out)
+
+    # Compute the number of numpy calls, over all, and by number of scipy functions.
+    df_np_calls = df.explode('np_calls')[["scipy_function","np_calls"]]
+    df_np_calls = df_np_calls[df_np_calls['np_calls'].notnull()]
+    df_np_calls = df_np_calls.explode("np_calls")
+
+    total_np_calls = df_np_calls.groupby("np_calls").size()
+    df_total_np_calls = pd.DataFrame({"total_np_calls": total_np_calls})
+
+    num_scipy_funs_used = df_np_calls.drop_duplicates().groupby("np_calls").size()
+    df_num_scipy_funs_used = pd.DataFrame({"num_scipy_funs_used_by":num_scipy_funs_used})
+
+    df_call_stats = df_num_scipy_funs_used.join(df_total_np_calls, lsuffix='_caller', rsuffix='_other')
+    df_call_stats = df_call_stats.sort_values(by=['num_scipy_funs_used_by', 'total_np_calls'], ascending=False)
+
+    my_out2 = "/home/amandapotts/git/functiondefextractor/data/scipy/np_call_stats.csv"
+    df_call_stats.to_csv(my_out2)
