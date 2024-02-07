2b0cde485f16f785b62fbad44fe4daf94afb079e
Amanda Potts <ajpotts@users.noreply.github.com>
2024-02-07 18:08:16 -0500

    working

--- functiondefextractor/extract_numpy.py
+++ functiondefextractor/extract_numpy.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
 
     df['np_calls'] = df['Code'].str.findall(r"np\.([\w\.]*)")
     df['num_np_calls'] = df['np_calls'].apply(lambda x: len(x))
-    df['np_calls_w_args'] = df['Code'].str.findall(r"np\.([\w\.]*\([\s\w=_]*\))")
+    df['np_calls_w_args'] = df['Code'].str.findall(r"np\.([\w_\.]+\(.*\))")
     df['scipy_function'] = df['Uniq ID'].str.findall(r"/home/amandapotts/git/scipy/scipy/([\w_/]*)")
     df = df.explode('scipy_function')
 
@@ -40,4 +40,27 @@ if __name__ == '__main__':
     df_call_stats = df_call_stats.sort_values(by=['num_scipy_funs_used_by', 'total_np_calls'], ascending=False)
 
     my_out2 = "/home/amandapotts/git/functiondefextractor/data/scipy/np_call_stats.csv"
+    df_call_stats.to_csv(my_out2)
+
+
+    df_args = df.explode('np_calls_w_args')
+    df_args = df_args[df_args['np_calls_w_args'].notnull()][["scipy_function", "np_calls_w_args"]]
+    df_args['np_call'] = df_args['np_calls_w_args'].str.findall(r"(^[\w\.]*)")
+    df_args = df_args.explode('np_call')
+    df_args['np_args'] = df_args['np_calls_w_args'].str.findall(r"([\w\s\._]*)=")
+    df_args = df_args.explode('np_args')
+    df_args = df_args[df_args['np_args'].notnull()]
+    df_args = df_args[["scipy_function", "np_call", "np_args"]]
+
+    total_np_calls = df_args.groupby(["np_call", "np_args"]).size()
+    df_total_np_calls = pd.DataFrame({"total_np_calls": total_np_calls})
+
+    num_scipy_funs_used = df_args.drop_duplicates().groupby(["np_call", "np_args"]).size()
+    df_num_scipy_funs_used = pd.DataFrame({"num_scipy_funs_used_by": num_scipy_funs_used})
+
+    df_call_stats = df_num_scipy_funs_used.join(df_total_np_calls, lsuffix='_caller', rsuffix='_other')
+    df_call_stats = df_call_stats.sort_values(by=['num_scipy_funs_used_by', 'total_np_calls'], ascending=False)
+
+
+    my_out3 = "/home/amandapotts/git/functiondefextractor/data/scipy/np_arg_stats.csv"
     df_call_stats.to_csv(my_out2)
