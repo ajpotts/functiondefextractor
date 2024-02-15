c20b2f2aed93db77e735ea1516dd572e2b60e493
Amanda Potts <ajpotts@users.noreply.github.com>
2024-02-15 15:35:50 -0500

    add arkouda

--- functiondefextractor/extract_numpy.py
+++ functiondefextractor/extract_numpy.py
@@ -1,12 +1,5 @@
-import sys
-
-from condition_checker import check_condition
-from core_extractor import extractor
-from core_extractor import get_report
-from extractor_cmd import validate_inputs
-from extractor_cmd import create_parser
-
 import pandas as pd
+from core_extractor import extractor, get_report
 
 
 def add_numpy_calls(df):
@@ -19,13 +12,14 @@ def add_numpy_calls(df):
 
 def add_library_functions(df: pd.DataFrame, lib_name: str, lib_path: str):
     df[lib_name + "_function"] = df["Uniq ID"].str.findall(
-        #r"/home/amandapotts/git/scipy/scipy/([\w_/]*)"
-        lib_path+"([\w_/]*)"
+        lib_path
+        + "([\w_/\.]*)"
     )
     df = df.explode(lib_name + "_function")
     return df
 
-def get_and_write_np_stats(lib_name:str,lib_path:str, out_dir:str):
+
+def get_and_write_np_stats(lib_name: str, lib_path: str, out_dir: str):
     df = extractor(lib_path)
     df = add_numpy_calls(df)
     df = add_library_functions(df, lib_name, lib_path)
@@ -33,16 +27,9 @@ def get_and_write_np_stats(lib_name:str,lib_path:str, out_dir:str):
     df.to_csv(my_out)
     return df
 
-if __name__ == "__main__":
-    out_dir = "/home/amandapotts/git/functiondefextractor/data/out/"
-    scipy_path = "/home/amandapotts/git/scipy/scipy"
-    pandas_path =  "/home/amandapotts/git/pandas/pandas"
 
-    #   Extract the function calls, and enhance with extracted function names.
-    df = get_and_write_np_stats("scipy", scipy_path, out_dir)
-
-    # Compute the number of numpy calls, over all, and by number of scipy functions.
-    df_np_calls = df.explode("np_calls")[["scipy_function", "np_calls"]]
+def get_numpy_calls_stats(df, lib_name: str, out_dir: str):
+    df_np_calls = df.explode("np_calls")[[lib_name + "_function", "np_calls"]]
     df_np_calls = df_np_calls[df_np_calls["np_calls"].notnull()]
     df_np_calls = df_np_calls.explode("np_calls")
 
@@ -50,35 +37,68 @@ if __name__ == "__main__":
     df_total_np_calls = pd.DataFrame({"total_np_calls": total_np_calls})
 
     num_scipy_funs_used = df_np_calls.drop_duplicates().groupby("np_calls").size()
-    df_num_scipy_funs_used = pd.DataFrame({"num_scipy_funs_used_by": num_scipy_funs_used})
+    df_num_scipy_funs_used = pd.DataFrame({"num_" + lib_name + "_funs_used_by": num_scipy_funs_used})
 
     df_call_stats = df_num_scipy_funs_used.join(df_total_np_calls, lsuffix="_caller", rsuffix="_other")
     df_call_stats = df_call_stats.sort_values(
-        by=["num_scipy_funs_used_by", "total_np_calls"], ascending=False
+        by=["num_" + lib_name + "_funs_used_by", "total_np_calls"], ascending=False
     )
 
-    my_out2 = out_dir + "scipy_np_call_stats.csv"
-    df_call_stats.to_csv(my_out2)
+    my_out = out_dir + lib_name + "_np_call_stats.csv"
+    df_call_stats.to_csv(my_out)
+    return df_call_stats
 
+
+def get_numpy_arg_stats(df: pd.DataFrame, lib_name: str, out_dir: str):
     df_args = df.explode("np_calls_w_args")
-    df_args = df_args[df_args["np_calls_w_args"].notnull()][["scipy_function", "np_calls_w_args"]]
+    df_args = df_args[df_args["np_calls_w_args"].notnull()][[lib_name + "_function", "np_calls_w_args"]]
     df_args["np_call"] = df_args["np_calls_w_args"].str.findall(r"(^[\w\.]*)")
     df_args = df_args.explode("np_call")
     df_args["np_args"] = df_args["np_calls_w_args"].str.findall(r"([\w\._]*)\s*=[^=<>!]")
     df_args = df_args.explode("np_args")
     df_args = df_args[df_args["np_args"].notnull()]
-    df_args = df_args[["scipy_function", "np_call", "np_args"]]
+    df_args = df_args[[lib_name + "_function", "np_call", "np_args"]]
 
     total_np_calls = df_args.groupby(["np_call", "np_args"]).size()
     df_total_np_calls = pd.DataFrame({"total_np_calls": total_np_calls})
 
     num_scipy_funs_used = df_args.drop_duplicates().groupby(["np_call", "np_args"]).size()
-    df_num_scipy_funs_used = pd.DataFrame({"num_scipy_funs_used_by": num_scipy_funs_used})
+    df_num_scipy_funs_used = pd.DataFrame({"num_" + lib_name + "_funs_used_by": num_scipy_funs_used})
 
     df_call_stats = df_num_scipy_funs_used.join(df_total_np_calls, lsuffix="_caller", rsuffix="_other")
     df_call_stats = df_call_stats.sort_values(
-        by=["num_scipy_funs_used_by", "total_np_calls"], ascending=False
+        by=["num_" + lib_name + "_funs_used_by", "total_np_calls"], ascending=False
     )
 
-    my_out3 = out_dir + "scipy_np_arg_stats.csv"
-    df_call_stats.to_csv(my_out3)
+    my_out = out_dir + lib_name + "_np_arg_stats.csv"
+    df_call_stats.to_csv(my_out)
+
+    return df_args
+
+
+def get_fuction_stats(df: pd.DataFrame, lib_name: str, out_dir: str):
+    df_abrev = df[[lib_name + "_function", "num_np_calls", "np_calls", "np_calls_w_args"]]
+    df_abrev = df_abrev.sort_values(by=["num_np_calls"], ascending=False)
+    df_abrev = df_abrev.reset_index()
+    my_out = out_dir + lib_name + "_function_stats.csv"
+    df_abrev.to_csv(my_out)
+    return df_abrev
+
+
+def run_stats(lib_name: str, lib_path:str, out_dir: str):
+    df = get_and_write_np_stats(lib_name, lib_path, out_dir)
+    df_call_stats = get_numpy_calls_stats(df, lib_name, out_dir)
+    df_args = get_numpy_arg_stats(df, lib_name, out_dir)
+    get_fuction_stats(df, lib_name, out_dir)
+    return df
+
+
+if __name__ == "__main__":
+    out_dir = "/home/amandapotts/git/functiondefextractor/data/out/"
+    scipy_path = "/home/amandapotts/git/scipy/scipy"
+    pandas_path = "/home/amandapotts/git/pandas/pandas"
+    arkouda_path = "/home/amandapotts/git/arkouda/arkouda"
+
+    run_stats("arkouda", arkouda_path, out_dir)
+    run_stats("scipy", scipy_path, out_dir)
+    run_stats("pandas", pandas_path, out_dir)
